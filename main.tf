ea582fb73ea7160734c03ea5c03cb0584416fa76
Mark Watts <mark@wattsonline.org>
2021-04-29 20:53:32 -0500

    subnetwork add

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = "0.14.9"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "3.64.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "3.64.0"
+    }
+  }
 }
 
 #################
@@ -11,7 +21,7 @@ resource "google_compute_network" "vpc_network" {
   mtu                             = var.mtu
   auto_create_subnetworks         = var.auto_create
   routing_mode                    = var.routing_mode
-  project                         = var.project
+  project                         = var.project_id
   delete_default_routes_on_create = var.delete_routes
 }
 
@@ -25,8 +35,4 @@ resource "google_compute_subnetwork" "subnetwork" {
   region                   = var.region
   network                  = google_compute_network.vpc_network.name
   private_ip_google_access = var.private_access
-  secondary_ip_range {
-    range_name    = "tf-test-secondary-range-update1"
-    ip_cidr_range = "192.168.10.0/24"
-  }
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,13 @@
 #########################
+# Google Info
+#########################
+
+variable "project_id" {
+  type        = string
+  description = "Project ID"
+}
+
+#########################
 # VPC Details
 #########################
 
@@ -41,3 +50,23 @@ variable "delete_routes" {
 # Subnetwork Details
 ####################
 
+variable "subnetwork_name" {
+  type        = string
+  description = "The name of the resource, provided by the client when initially creating the resource. The name must be 1-63 characters long, and comply with RFC1035."
+}
+
+variable "cidr_range" {
+  type        = string
+  description = "The range of internal addresses that are owned by this subnetwork. Provide this property when you create the subnetwork. For example, 10.0.0.0/8 or 192.168.0.0/16. Ranges must be unique and non-overlapping within a network. Only IPv4 is supported."
+}
+
+variable "region" {
+  type        = string
+  description = " The GCP region for this subnetwork."
+}
+
+variable "private_access" {
+  type        = bool
+  description = "When enabled, VMs in this subnetwork without external IP addresses can access Google APIs and services by using Private Google Access."
+  default     = false
+}
